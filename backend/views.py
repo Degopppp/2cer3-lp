1737b95e77861e3721ad23a926278f98f0e80a74
degop <degop@debian.lp>
2024-06-25 13:47:04 -0400

    Working first endpoints

---
+++ backend/views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets , generics
+from backend.models import Number
+from backend.serializers.NumberSerializer import NumberSerializer
+
+import random
+import string
+
+class NumberViewSet(viewsets.ModelViewSet):
+    queryset = Number.objects.all()
+    serializer_class = NumberSerializer
+    
+    def get_queryset(self):
+        queryset = Number.objects.all()
+        number = self.request.query_params.get('number', None)
+        if number is not None:
+            queryset = queryset.filter(number=number)
+        return queryset
+
+class CreateRandomNumber(generics.CreateAPIView):
+    serializer_class = NumberSerializer
+
+    def perform_create(self, serializer):
+        random_number = random.randint(1, 100)
+        random_letter = random.choice(string.ascii_uppercase)
+        serializer.save(number=random_number, letter=random_letter)
--- cer3/settings.py
+++ cer3/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$8&6r=%tn8ik=&--_7@^dke9(d0xvz66j2ux&p*8sazhjw_nl8'
+SECRET_KEY = 'django-insecure-4^b3v_!135!1*dl%%si6&h25fw1&=r#=qej5q-a2c_0-qspgav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'backend',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -73,11 +77,9 @@ WSGI_APPLICATION = 'cer3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://postgres:miau123.@cer3_db:5432/cer3')
 }
 
 
